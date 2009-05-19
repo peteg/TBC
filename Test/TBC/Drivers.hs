6bc80f6707f1d37e729a33aa1ac7a237b47c1f3a
Mark Wotton <mwotton@loki.local>
2009-05-19 11:30:37 +1000

    remove tracing

--- Test/TBC/Drivers.hs
+++ Test/TBC/Drivers.hs
@@ -20,8 +20,6 @@ import System.Exit
 import System.IO -- ( hClose, hFlush, hGetContents, hPutStr )
 import System.Process ( runInteractiveProcess, waitForProcess )
 
-import Debug.Trace
-
 -------------------------------------------------------------------
 
 -- Drivers
@@ -63,7 +61,7 @@ ghci verbose cmd flags =
      return $ MkDriver
                 { hci_send_cmd = ghci_sync verbose hin hout
                 , hci_load_file = load_file
-                , hci_close = trace "hci_close" $ hPutStr hin ":quit\n" >>  hFlush hin >> waitForProcess hpid
+                , hci_close = hPutStr hin ":quit\n" >>  hFlush hin >> waitForProcess hpid
                 }
 
 ghci_sync :: Bool -- ^ Verbosity
